b6620b970e43df435457b13f5069bd2267e31f5d
unknown <you@example.com>
2019-11-05 11:28:01 -0600

    Ultimos ajustes

--- app/src/main/java/com/example/crudmysqlandroid/Config.java
+++ app/src/main/java/com/example/crudmysqlandroid/Config.java
@@ -2,7 +2,7 @@ package com.example.crudmysqlandroid;
 
 public class Config {
 
-    /*
+/*
     //URL PARA ACCESO REMOTA A BASE DE DATOS MYSQL DEL SERVIDOR DEL PROF. GÁMEZ EN LA NUBE.
     public static final String urlGuardar = "http://mjgl.com.sv/mysqlcrud/guardar.php";                               //Guardar datos DB.
     public static final String urlEliminar = "http://mjgl.com.sv/mysqlcrud/eliminar.php";                             //Eliminar datos DB.
@@ -14,10 +14,11 @@ public class Config {
     public static final String urlConsultaCodigo = "http://mjgl.com.sv/mysqlcrud/buscarArticulosCodigo.php";            //Busquedas por código.
     public static final String urlConsultaDescripcion = "http://mjgl.com.sv/mysqlcrud/buscarArticulosDescripcion.php";  //Busquedas por descripción.
     public static final String urlConsultaAllArticulos = "http://mjgl.com.sv/mysqlcrud/buscarArticulos.php";            //Devuelve todos los registros de la tabla MySQL.
-    */
 
-    //URL PARA ACCESO A BASE DE DATOS LOCAL MYSQL.
+
+    /*URL PARA ACCESO A BASE DE DATOS LOCAL MYSQL.
     //public static final String urlGuardar = "http://localhost/conexion/guardar.php";                               //Guardar datos DB.
+    */
     public static final String urlGuardar = "http://localhost/conexion/guardar.php";                               //Guardar datos DB.
 
     public static final String urlEliminar = "http://localhost/conexion/eliminar.php";                             //Eliminar datos DB.
